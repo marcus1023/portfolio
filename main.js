f9c7a1efb762ca5ecfccebfe95e447c73c624291
Marcus Ogden <marcus@userlite.com>
2016-12-17 11:38:52 -0700

    these are the new additions

---
+++ main.js
@@ -0,0 +1,14 @@
+$('document').ready(function() {
+
+// $('.mini-flex-section')onClick(function(){
+  // $('.spacer-quote').hide();
+// })
+
+
+
+$('.mini-flex-section img').on('click', function() {
+  $('.spacer-quote').toggleClass( "shower" );
+})
+
+
+});
