2819ef22c8dbfa50167aa9beb319f46835c723d7
Marcus Ogden <marcusogden@Marcuss-MBP.PK5001Z>
2017-04-13 04:19:17 -0400

    MVP

--- JS/angular/app.js
+++ JS/angular/app.js
@@ -6,6 +6,11 @@ angular.module('portfolio',  ['ui.router'])
             templateUrl: "./JS/angular/templates/home.html",
             controller: "userController"
         })
+        .state('home.piece',{
+            url:'/piece',
+            templateUrl: "./JS/angular/templates/profilePiece.html",
+            controller: "userController"
+        })
 
         $urlRouterProvider
             .when('/', '/home')
--- JS/angular/controllers/homeController.js
+++ JS/angular/controllers/homeController.js
@@ -37,4 +37,45 @@ $scope.sendMail = function(message){
 }
 
 
+$scope.defineProject = function(project){
+  if(project === 'triad'){
+    $scope.currentProject = {
+      title: 'Triad PC Clinic',
+      link: 'https://www.triadpcclinic.com/',
+      image1: 'portbgTriad',
+      image2: 'portbgTriad2',
+      image3: 'portbgTriad3',
+      tech: 'React JS, Userlite CMS, Ajax,CSS, HTML,Node JS, SQL',
+      disc: 'This project was for a Raleigh Durham based tech and security firm and is a simple website allowing users to discover thier products, make payments, and facilitate client interaction.'
+    }
+  }
+  if(project === 'cash'){
+    $scope.currentProject = {
+      title: 'Cash Analytics',
+      link: 'https://www.getcashanalytics.com/',
+      image1: 'cashAnalytics2',
+      image2: 'cashAnalytics2',
+      image3: 'cashAnalytics3',
+      tech: 'Angular JS, Userlite CMS, Ajax, PHP, Bootstrap, CSS, HTML, Node JS, SQL',
+      disc: 'This major overnight investment analytics company is a major player in the investment analytics scene. This site is designed with a marketing funnel startiegy tooled to point clients in the right direction.'
+    }
+  }
+  if(project === 'electric'){
+    $scope.currentProject = {
+      title: 'Guerrera Electric',
+      link: 'http://www.guerreraelectric.com/',
+      image1: 'electric',
+      image2: 'electric2',
+      image3: 'electric3',
+      tech: 'Angular JS, Userlite CMS, Ajax, PHP, Bootstrap, CSS, HTML, Node JS, SQL',
+      disc: 'Guerrera Electric is a main player in the PA electric ulitlity repair and installment scene. The blog associated with this site in componation with a rich site map made this project a bit challangeing. As always, however, I ended up with a very satisfied customer'
+    }
+  }
+  if(project === 'construct'){
+    $scope.currentProject = {
+      title: 'Under Construction'
+    }
+  }
+}
+
 })
